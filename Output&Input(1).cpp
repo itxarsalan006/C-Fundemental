78d100ed793964e8732a18f43aa1cc3da15f6deb
itxarsalan006 <marsalan8976@gmail.com>
2024-07-11 03:05:18 -0700

    Add files via upload

---
+++ Output&Input(1).cpp
@@ -0,0 +1,11 @@
+O#include<iostream>
+using namespace std;
+int main() {
+	int number;
+	cout<<"Please enter a number"<<endl; //For taking Output 
+	cin>>number; //For taking Input
+	cout<<"The number is : "<<number<<endl;
+	
+	
+	return 0;
+}
